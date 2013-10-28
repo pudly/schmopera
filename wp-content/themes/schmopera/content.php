8fa1e537dca7ae4ddde3a4f9da43a0ccb5172dd8
Andrej <andrej@pud.ca>
2013-10-27 22:08:07 -0400

    Refactoring and header adjustments

--- wp-content/themes/schmopera/content.php
+++ wp-content/themes/schmopera/content.php
@@ -39,14 +39,13 @@
 			if ($attachments) {
 				foreach ($attachments as $attachment) {
 					if(wp_attachment_is_image( $attachment->ID )) {
-						echo wp_get_attachment_image_src( $attachment->ID, false )[0];
+						echo wp_get_attachment_image_src( $attachment->ID, false)[0];
 						break;
 					}
 				}
 			}
 			
-		?>
-"></a>								  
+		?>"></a>								  
 		<?php echo wp_trim_words( get_the_content(), 100 ); ?>
 		
 		<?php
